1cea87948e48f40d39c91f57871832c2bff27f40
Xingwang Liu <cnldlxw@163.com>
2018-07-16 08:33:31 +0800

    Update golang.el

--- personal/preload/golang.el
+++ personal/preload/golang.el
@@ -1,12 +1,3 @@
-(defconst EMACS_CONF "/Users/liuxingwang/.emacs.d/")
-; emacs中读取系统环境变量的插件，否则在emacs中执行go run之类的变量无法找到
-(load-file (concat EMACS_CONF "elpa/exec-path-from-shell-20180224.1916/exec-path-from-shell.el"))
-(when (fboundp 'exec-path-from-shell-copy-env)
-         (exec-path-from-shell-copy-env "GOPATH")
-                (exec-path-from-shell-copy-env "GOBIN")
-                         (exec-path-from-shell-copy-env "GOROOT")
-                                    (exec-path-from-shell-copy-env "PATH"))
-
 ; go跳回定义快捷键
 (global-set-key (kbd "C-c C-b") #'pop-tag-mark)
 
@@ -14,17 +5,6 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-
-; go-autocomplete
-(load-file (concat EMACS_CONF "elpa/auto-complete-20170124.1845/auto-complete-autoloads.el"))
-(load-file (concat EMACS_CONF "elpa/popup-20160709.729/popup.el"))
-(load-file (concat EMACS_CONF "elpa/go-autocomplete-20170626.323/go-autocomplete.el"))
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories (concat EMACS_CONF "elpa/auto-complete-20170124.1845/dict"))
-  (ac-config-default)
-(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-
 
 ; emacs主题设置，刚安装好的emacs没有atom-one-dark，会引起出错 需要去package-list-packages中寻找主题安装
 (setq prelude-theme 'atom-one-dark)
@@ -41,4 +21,4 @@
 
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
-(provide 'golang)
+(provide 'mygolang)
